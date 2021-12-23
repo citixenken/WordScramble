1bb8c3df703cca70a2c26cd06ddc5d0744988a91
citixenken <kmuyesu@gmail.com>
2021-12-23 22:14:05 +0300

    initial commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ken Muyesu on 23/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
